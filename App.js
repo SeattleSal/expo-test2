89b2ea78a840691e8a5a0e693cf8507c1a89a909
Sally Perez <68660009+SeattleSal@users.noreply.github.com>
2021-04-27 18:06:48 -0700

    adding basic components Image and button

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import sunset from "./assets/Door-a.jpg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={sunset} style={styles.logo} />
+
+      <Text style={styles.instructions}>Hello World!</Text>
+
+      <Image
+        source={{ uri: "https://reactjs.org/logo-og.png" }}
+        style={{ width: 400, height: 400 }}
+      />
+
+      <TouchableOpacity
+        onPress={() => alert("Hello World!")}
+        style={{ backgroundColor: "blue" }}
+      >
+        <Text style={{ fontSize: 20, color: "#fff" }}>Pick a photo!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +28,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 300,
+    height: 150,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 24,
+    marginHorizontal: 15,
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # expo-test2
+
+Creating a React Native project with Expo (https://docs.expo.io/)
+
+Install Expo globally:
+npm install --global expo-cli
